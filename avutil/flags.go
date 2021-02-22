6477b43c86e8293498635b7f6b2c5f34f78f3c89
zimwip <tho.zimmer@gmail.com>
2021-02-22 09:21:39 +0100

    Add sample format

--- avutil/flags.go
+++ avutil/flags.go
@@ -8,6 +8,7 @@ package avutil
 
 //#cgo pkg-config: libavformat libavcodec libavutil libavdevice libavfilter libswresample libswscale
 //#include <libavutil/channel_layout.h>
+//#include <libavutil/samplefmt.h>
 import "C"
 
 const (
@@ -81,4 +82,23 @@ const (
 	AV_CH_LAYOUT_HEXADECAGONAL     = int(C.AV_CH_LAYOUT_HEXADECAGONAL)
 	AV_CH_LAYOUT_STEREO_DOWWNMIX   = int(C.AV_CH_LAYOUT_STEREO_DOWNMIX)
 	//AV_CH_LAYOUT_22POINT2          = int(C.AV_CH_LAYOUT_22POINT2)
+)
+
+const (
+	AV_SAMPLE_FMT_NONE = int(C.AV_SAMPLE_FMT_NONE)
+	AV_SAMPLE_FMT_U8   = int(C.AV_SAMPLE_FMT_U8)
+	AV_SAMPLE_FMT_S16  = int(C.AV_SAMPLE_FMT_S16)
+	AV_SAMPLE_FMT_S32  = int(C.AV_SAMPLE_FMT_S32)
+	AV_SAMPLE_FMT_FLT  = int(C.AV_SAMPLE_FMT_FLT)
+	AV_SAMPLE_FMT_DBL  = int(C.AV_SAMPLE_FMT_DBL)
+
+	AV_SAMPLE_FMT_U8P  = int(C.AV_SAMPLE_FMT_U8P)
+	AV_SAMPLE_FMT_S16P = int(C.AV_SAMPLE_FMT_S16P)
+	AV_SAMPLE_FMT_S32P = int(C.AV_SAMPLE_FMT_S32P)
+	AV_SAMPLE_FMT_FLTP = int(C.AV_SAMPLE_FMT_FLTP)
+	AV_SAMPLE_FMT_DBLP = int(C.AV_SAMPLE_FMT_DBLP)
+	AV_SAMPLE_FMT_S64  = int(C.AV_SAMPLE_FMT_S64)
+	AV_SAMPLE_FMT_S64P = int(C.AV_SAMPLE_FMT_S64P)
+
+	AV_SAMPLE_FMT_NB = int(C.AV_SAMPLE_FMT_NB)
 )
