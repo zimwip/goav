8aafc661d4ec16cec0abbbcb443fa9d276f4b32d
zimwip <tho.zimmer@gmail.com>
2021-02-22 17:52:19 +0100

    Fix

--- avutil/imgutils.go
+++ avutil/imgutils.go
@@ -17,9 +17,11 @@ const (
  * Return the name of sample_fmt, or NULL if sample_fmt is not
  * recognized.
  */
-func AvImageFillArrays(srcData [8]*uint8, dst_linesize [8]int32, src *uint8, pix_fmt AvPixelFormat, width, height int, align int) int {
-	cdata := (**C.uint8_t)(unsafe.Pointer(&srcData[0]))
-	cdst_linesize := (*C.int)(unsafe.Pointer(&dst_linesize[0]))
+func AvImageFillArrays(f *Frame, srcData [8]*uint8, dst_linesize [8]int32, src *uint8, pix_fmt AvPixelFormat, width, height int, align int) int {
+	//cdata := (**C.uint8_t)(unsafe.Pointer(&srcData[0]))
+	//cdst_linesize := (*C.int)(unsafe.Pointer(&dst_linesize[0]))
+	cdata := (**C.uint8_t)(unsafe.Pointer(&f.data[0]))
+	cdst_linesize := (*C.int)(unsafe.Pointer(&f.linesize[0]))
 	csrc := (*C.uint8_t)(unsafe.Pointer(src))
 	return int(C.av_image_fill_arrays(cdata, cdst_linesize, csrc, (C.enum_AVPixelFormat)(pix_fmt), C.int(width), C.int(height), C.int(align)))
 }
